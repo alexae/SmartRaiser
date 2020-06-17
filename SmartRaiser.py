8233897cd4d0a187c6c1fdf8574f0d8051fdea02
alexae <mail@aesch.li>
2020-06-17 11:51:44 +0200

    API in Phyton eingefügt
    API ist beschrieben und mit Statischen werten befüllt um Tests durchzuführen.
    Diese sollen gelöscht werden für die definitive Lösung.

--- SmartRaiser.py
+++ SmartRaiser.py
@@ -1,22 +1,51 @@
-from flask import Flask, url_for
+from flask import Flask
 from markupsafe import escape
 from flask import jsonify
 
 app = Flask(__name__)
+#Statische Werte zum Testen der API -> Löschen nach ferigstellung
+OnOff = True            #boolean
+humidity = 40           #Int
+waterlevel = 0          #int
+chargelevel = 0         #Int
+wateringtime = 00:05    #String mm:ss
 
+# Bis hier löschen
 
-@app.route('/running' , methods=['GET', 'PUT'])
+@app.route('/running', methods=['GET', 'PUT'])
 def running():
-    return 'OnOff'
+    # Programm Code hier einfügen
+    #
+    #
+    return OnOff
 
 @app.route('/humidity' , methods=['GET', 'PUT'])
 def humidity():
-    return 'humidity'
+    # Programm Code hier einfügen
+    #
+    #
+    return humidity
 
-@app.route('/user/<username>' , methods=['GET', 'PUT'])
-def profile(username):
-    return '{}\'s profile'.format(escape(username))
-
+@app.route('/waterlevel' , methods=['GET'])
+def waterlevel():
+    # Programm Code hier einfügen
+    #
+    #
+    return waterlevel
+    
+@app.route('/chargelevel' , methods='GET')
+def chargelevel():
+    # Programm Code hier einfügen
+    #
+    #
+    return chargelevel
+    
+@app.route('/wateringtime' , methods='GET', 'PUT')
+def wateringtime():
+    # Programm Code hier einfügen
+    #
+    #
+    return wateringtime
     
 if __name__ == '__main__':
-    app.run(debug=False,host="0.0.0.0")
+    app.run(debug=True,host="0.0.0.0")
